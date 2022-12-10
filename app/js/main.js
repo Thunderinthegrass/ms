b8f846be5f8a292f085b605ab33a70c12b212066
ThunderInTheGrass <propeller8924@gmail.com>
2022-12-10 15:04:20 +0800

    h

--- app/js/main.js
+++ app/js/main.js
@@ -26,68 +26,76 @@ const newsSlider = new Swiper(".news__slider", {
 // }
 // mobileMenu();
 
-function popupForm() {
-  const btns = document.querySelectorAll(".modal-btn");
-  const modalOverlay = document.querySelector(".modal-overlay ");
-  const modals = document.querySelectorAll(".modal");
-  const modalCloseBtn = document.querySelector(".modal__close-btn");
-  const close = document.querySelectorAll(".close");
+// function popupForm() {
+//   const btns = document.querySelectorAll(".modal-btn");
+//   const modalOverlay = document.querySelector(".modal-overlay ");
+//   const modals = document.querySelectorAll(".modal");
+//   const modalCloseBtn = document.querySelector(".modal__close-btn");
+//   const close = document.querySelectorAll(".close");
 
-  btns.forEach((el) => {
-    el.addEventListener("click", (e) => {
-      let path = e.currentTarget.getAttribute("data-path");
+//   btns.forEach((el) => {
+//     el.addEventListener("click", (e) => {
+//       let path = e.currentTarget.getAttribute("data-path");
 
-      modals.forEach((el) => {
-        el.classList.remove("modal--visible");
-      });
+//       modals.forEach((el) => {
+//         el.classList.remove("modal--visible");
+//       });
 
-      document
-        .querySelector(`[data-target="${path}"]`)
-        .classList.add("modal--visible");
-      modalOverlay.classList.add("modal-overlay--visible");
-    });
-  });
+//       document
+//         .querySelector(`[data-target="${path}"]`)
+//         .classList.add("modal--visible");
+//       modalOverlay.classList.add("modal-overlay--visible");
+//     });
+//   });
 
-  modalOverlay.addEventListener("click", (e) => {
-    console.log(e.target);
+//   modalOverlay.addEventListener("click", (e) => {
+//     console.log(e.target);
 
-    close.forEach((elem) => {
-      if (e.target == modalOverlay || e.target == elem) {
-        modalOverlay.classList.remove("modal-overlay--visible");
-        modals.forEach((el) => {
-          el.classList.remove("modal--visible");
-        });
-      }
-    })
-  });
-}
-popupForm();
+//     close.forEach((elem) => {
+//       if (e.target == modalOverlay || e.target == elem) {
+//         modalOverlay.classList.remove("modal-overlay--visible");
+//         modals.forEach((el) => {
+//           el.classList.remove("modal--visible");
+//         });
+//       }
+//     })
+//   });
+// }
+// popupForm();
 
-function moreBtn() {
-  let btn = document.querySelector('.members__more-btn');
-  let membersInnerMore = document.querySelector('.members__inner_more');
-  let span = btn.querySelector('span')
+// function moreBtn() {
+//   let btn = document.querySelector('.members__more-btn');
+//   let membersInnerMore = document.querySelector('.members__inner_more');
+//   let span = btn.querySelector('span')
 
-  btn.addEventListener('click', () => {
-    membersInnerMore.classList.toggle('d-none');
-    membersInnerMore.classList.toggle('d-flex');
-    span.classList.toggle('after-rotate');
+//   btn.addEventListener('click', () => {
+//     membersInnerMore.classList.toggle('d-none');
+//     membersInnerMore.classList.toggle('d-flex');
+//     span.classList.toggle('after-rotate');
 
-    if (membersInnerMore.classList.contains('d-flex')) {
-      span.innerHTML = 'Скрыть';
-    }
-    else {
-      span.innerHTML = 'Больше';
-    }
-  })
+//     if (membersInnerMore.classList.contains('d-flex')) {
+//       span.innerHTML = 'Скрыть';
+//     }
+//     else {
+//       span.innerHTML = 'Больше';
+//     }
+//   })
 
-}
-moreBtn();
+// }
+// moreBtn();
 
 // function tabs() {
 
 //   let tabItem = document.querySelectorAll('.map__info');
 //   let tabNavItem = document.querySelectorAll('.map__dot');
+//   let closeBtn = document.querySelectorAll('.close-info-btn');
+
+//   closeBtn.forEach((elem, index) => {
+//     elem.addEventListener('click', () => {
+//       tabItem[index].classList.remove('tab--active');
+//       tabNavItem[index].classList.remove('tabs-navigation__item--active');
+//     })
+//   })
 
 //   function tab(tabItem, tabNavItem) {
 //     tabNavItem.forEach((elem, index) => {
@@ -104,6 +112,8 @@ moreBtn();
 //   }
   
 //   tab(tabItem, tabNavItem);
+
+
 // }
 // tabs();
 
