fa31c2d1ab0851cf5cf75c8f9ec2794490eaea30
ThunderInTheGrass <propeller8924@gmail.com>
2022-12-14 21:08:39 +0800

    vjgfkgjtu

--- app/js/main.js
+++ app/js/main.js
@@ -202,4 +202,12 @@ function scrollToLink() {
 scrollToLink();
 
 
+function registrationForm() {
+  const form = document.querySelector('.registration-form');
+  const telSelector = form.querySelector('input[type="tel"]');
+  const inputMask = new Inputmask('+7 (999) 999-99-99');
+  inputMask.mask(telSelector )
+}
+
+
 
